629145b5affef4ae4559e2886a72f55b698102b2
chennalhua <vip899629@gmail.com>
2021-04-04 15:57:34 +0800

    axios 發出網路請求

--- js/all.js
+++ js/all.js
@@ -8,38 +8,16 @@
 // 星級區間是 1-10 分
 // 金額、組數、星級的 type 為 Number
 
-let data = [
-    {
-      "id": 0,
-      "name": "肥宅心碎賞櫻3日",
-      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-      "area": "高雄",
-      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-      "group": 87,
-      "price": 1400,
-      "rate": 10
-    },
-    {
-      "id": 1,
-      "name": "貓空纜車雙程票",
-      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台北",
-      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-      "group": 99,
-      "price": 240,
-      "rate": 2
-    },
-    {
-      "id": 2,
-      "name": "台中谷關溫泉會1日",
-      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台中",
-      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-      "group": 20,
-      "price": 1765,
-      "rate": 7
-    }
-];
+let data;
+
+//axios
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json') // ( /user/12345 ) 網址
+	//請求成功就會執行該 function
+  .then(function (response) {
+    console.log('資料已回傳');
+    data = response.data.data;
+    init();
+  });
 
 //選擇器
 const areaList = document.querySelector('.area-list');
@@ -55,9 +33,16 @@ const ticketDes = document.querySelector('#des');
 const form = document.querySelector('form');
 //表單按鈕
 const addBtn = document.querySelector('.add-btn');
+const filterLocal = document.querySelector('#local');
 
 //初始化
 function init(){
+  listContentBox();
+  filterLocal.value == '全部';
+  searchNum.textContent =`本次搜尋共 ${data.length} 筆資料`;
+}
+
+function listContentBox(){
   let str = '';
   data.forEach(function(item){
     let content = `<li class="area-card">
@@ -82,13 +67,10 @@ function init(){
     str += content;
   });
   areaList.innerHTML = str;
-  searchNum.textContent =`本次搜尋共 ${data.length} 筆資料`;
 }
-init();
 
 //篩選器
 function filterBox(){
-  const filterLocal = document.querySelector('#local');
   filterLocal.addEventListener('change',function(e){
     let str = '';
     let count = 0;
@@ -129,7 +111,7 @@ filterBox();
 //新增表單
 function addTicketBox(){
   addBtn.addEventListener('click',function(e){
-    if(ticketLevel.value > 10){
+    if(ticketLevel.value > 10 || ticketLevel.value <= 0){
       alert('星級區間是 1-10 分，請重新填寫')
       form.reset(); //清除表單內容
       return
